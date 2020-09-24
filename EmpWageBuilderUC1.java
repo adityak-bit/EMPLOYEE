0affd5e1bf2272d02a6c0c9140b80fa555596e5d
adityak-bit <71621023+adityak-bit@users.noreply.github.com>
2020-09-24 13:16:03 +0530

    Add files via upload

---
+++ EmpWageBuilderUC1.java
@@ -0,0 +1,15 @@
+package com.BridgeLabz;
+
+public class EmpWageBuilderUC1 {
+	public static void main(String[] args) {
+		//Constants
+		int IS_FULL_TIME = 1;
+		//Computation
+		double empCheck = Math.floor(Math.random() *10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
+
+}
